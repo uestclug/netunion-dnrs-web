960116d0e87f33f720874c3d0edf84e7f81e9748
宋璟珅 <lolipop0703@qq.com>
2020-02-18 23:31:18 +0800

    add Footer and Navbar

--- netunion-dormitory-network-repair-system/src/main.js
+++ netunion-dormitory-network-repair-system/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min.js'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
 
