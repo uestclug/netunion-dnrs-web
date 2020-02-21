8d04c0619b43fe70d7c03b9e7d7e6b9cb33c2983
宋璟珅 <lolipop0703@qq.com>
2020-02-21 13:43:15 +0800

    init database

---
+++ netunion-dormitory-network-repair-system/server/index.js
@@ -0,0 +1,4 @@
+// 配置后端端口和 api 路由
+const logApi = require('./api/logApi')
+const orderApi = require('./api/orderApi')
+const userApi = require('./api/userApi')
---
+++ netunion-dormitory-network-repair-system/server/sqlMap.js
@@ -0,0 +1,6 @@
+// 存放 MySQL 函数
+var sqlMap = {
+
+}
+
+module.exports = sqlMap
